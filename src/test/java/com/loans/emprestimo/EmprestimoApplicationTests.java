f769c4b48c988fdca29e650c294231c1e003ad85
bispobr <156684382+bispobr@users.noreply.github.com>
2025-02-21 10:04:58 -0300

    Initial commit

---
+++ src/test/java/com/loans/emprestimo/EmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loans.emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
