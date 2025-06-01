6f7dc86a5fb0b9a846fe287710a5f046fdeb12e8
bispobr <156684382+bispobr@users.noreply.github.com>
2025-06-01 10:44:15 -0300

    feat: adiciona logs e documentação da api

--- src/main/java/com/loans/emprestimo/controller/EmprestimoController.java
+++ src/main/java/com/loans/emprestimo/controller/EmprestimoController.java
@@ -3,6 +3,10 @@ package com.loans.emprestimo.controller;
 import com.loans.emprestimo.dto.ClienteRequisicaoDTO;
 import com.loans.emprestimo.dto.EmprestimoRespostaDTO;
 import com.loans.emprestimo.services.EmprestimoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/customer-loans")
 public class EmprestimoController {
@@ -21,12 +26,16 @@ public class EmprestimoController {
     private EmprestimoService emprestimoService;
 
     @PostMapping
-    public Map<String, Object> getEmpretimoDisponivel(@RequestBody ClienteRequisicaoDTO clienteRequisicaoDTO){
-
+    @Operation(description = "Endpoint responsável por determinar as modalidades de empréstimos disponíveis")
+    @ApiResponse(responseCode = "200", description = "modalidades de empréstimos com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição, dados enviados não atendem os requisitos")
+    @ApiResponse(responseCode = "500", description = "Erro interno")
+    public Map<String, Object> getEmpretimoDisponivel(@Valid @RequestBody ClienteRequisicaoDTO clienteRequisicaoDTO){
+        log.info("requisição para definir novo emprestimo recebida cliente:" + clienteRequisicaoDTO.name());
         List<EmprestimoRespostaDTO> emprestimo = emprestimoService.definirEmprestimo(clienteRequisicaoDTO);
 
         Map<String,Object> resposta =  new HashMap<>();
-        resposta.put("customer", clienteRequisicaoDTO.getName());
+        resposta.put("customer", clienteRequisicaoDTO.name());
         resposta.put("loans",emprestimo);
 
         return resposta;
