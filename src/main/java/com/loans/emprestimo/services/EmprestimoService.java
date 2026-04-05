caf6aeb51874a26d6744a5f063c5a57dfcf7fb23
bispobr <156684382+bispobr@users.noreply.github.com>
2026-04-05 10:07:48 -0300

    refactor : reestruturação de metodos

--- src/main/java/com/loans/emprestimo/services/EmprestimoService.java
+++ src/main/java/com/loans/emprestimo/services/EmprestimoService.java
@@ -2,39 +2,58 @@ package com.loans.emprestimo.services;
 
 
 import com.loans.emprestimo.dto.ClienteRequisicaoDTO;
-import com.loans.emprestimo.dto.EmprestimoRespostaDTO;
+import com.loans.emprestimo.dto.DTO;
+import com.loans.emprestimo.mapper.CustomerMapper;
+import com.loans.emprestimo.model.Customer;
+import com.loans.emprestimo.model.LoanType;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class EmprestimoService {
 
-    public List<EmprestimoRespostaDTO> definirEmprestimo(ClienteRequisicaoDTO clienteRequisicaoDTO){
+    @Autowired
+    private CustomerMapper customerMapper;
 
-        int age = clienteRequisicaoDTO.age();
-        double income = clienteRequisicaoDTO.income();
-        String location = clienteRequisicaoDTO.location();
+    public Map<String,Object> definirEmprestimo(ClienteRequisicaoDTO clienteRequisicaoDTO){
 
-        List<EmprestimoRespostaDTO> emprestimo =  new ArrayList<>();
+        Customer cliente = customerMapper.paraCustomer(clienteRequisicaoDTO);
 
-        if (income <= 3000){
-            emprestimo.add(new EmprestimoRespostaDTO("PERSONAL",4));
-            emprestimo.add(new EmprestimoRespostaDTO("GUARANTEED",3));
+        List<DTO> emprestimo = new ArrayList<>();
+
+        List<DTO> resultados = modalidades(cliente,emprestimo);
+
+        Map<String,Object> emprestimosConcedidos = new HashMap<>();
+        emprestimosConcedidos.put("loans",resultados);
+        emprestimosConcedidos.put("customer", clienteRequisicaoDTO.name());
+        log.info("modalidades de empréstimo definida"); return emprestimosConcedidos;
+    }
+
+    public List<DTO> modalidades (Customer cliente, List<DTO> emprestimo ){
+        if (cliente.getIncome() > 0 & cliente.getIncome() <= 3000){
+            emprestimo.add(new DTO(LoanType.PERSONAL,4));
+            emprestimo.add(new DTO(LoanType.GUARANTEED,3));
+        }
+        if (cliente.getIncome() >= 5000){
+            emprestimo.add(new DTO(LoanType.CONSIGNMENT,2));
         }
 
-        if (income >= 5000){
-            emprestimo.add(new EmprestimoRespostaDTO("CONSIGMENT",2));
+        if(cliente.getIncome() >= 3000 && cliente.getIncome() <= 5000 && cliente.getAge() < 30 && "SP".equalsIgnoreCase(cliente.getLocation())){
+            emprestimo.add(new DTO(LoanType.PERSONAL,4));
+            emprestimo.add(new DTO(LoanType.GUARANTEED,3));
         }
 
-        if(income >= 3000 && income <= 5000 && age < 30 && "SP".equalsIgnoreCase(location)){
-            emprestimo.add(new EmprestimoRespostaDTO("PERSONAL",4));
-            emprestimo.add(new EmprestimoRespostaDTO("GUARANTEED",3));
+        if (emprestimo.isEmpty()){
+            emprestimo.add(new DTO(LoanType.NENHUM,0));
+            return emprestimo;
         }
-        log.info("modalidades de empréstimo definida");
-        return  emprestimo;
+        return emprestimo;
     }
 }
